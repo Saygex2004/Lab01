3ff07399ff4e4f9eaf8e8ce297e26675b6f2ffda
Saygex2004 <borodasc@gmail.com>
2025-02-20 15:08:46 +0100

    code complete

--- main.py
+++ main.py
@@ -5,23 +5,31 @@ import random
 #creo una funzione gioco
 def game():
     options = ["rock", "paper", "scissor"]
-
     #permetto all'utente di scegliere la mossa o di uscire
     while True:
         print("choose: rock, paper, scissor or exit to quit")
-        user_choice = input("Your choice: ")
+        while True:
+            user_choice = input("Your choice: ")
+            if user_choice == "exit":
+                print("You left the game!")
+                return
+            if user_choice not in options:
+                print("Invalid choice")
+            else:
+                break
 
-        computer_choice = random.choice["rock", "paper", "scissor"]
+        computer_choice = random.choice(options)
+        print(f"Computer choice is:", computer_choice)
 
-        if user_choice == "rock":
-            ...
+        if user_choice == computer_choice:
+            print("Tie")
 
-        elif user_choice == "paper":
-            ...
+        elif (user_choice == "paper" and computer_choice == "rock") or (user_choice == "scissor" and computer_choice == "paper") or (user_choice == "rock" and computer_choice == "scissor"):
+            print("User won")
 
-        elif user_choice == "scissor":
-            ...
-
+        else:
+            print("Computer won")
+game()
 
 #eseguo la scelta automatica del computer
 
