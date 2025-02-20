ebf4e49fa902594d89c15faf634606234e96bd91
AnnaOccoffer <annaoccoffer@gmail.com>
2025-02-20 12:46:54 +0100

    Update main.py
    add game of:  rock, paper, scissors

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-x = 10 
-x = x + 4
+# sasso, carta, forbice 
+
+#creo una funzione gioco
+#permetto all'utente di scegliere la mossa o di uscire
+
+#eseguo la scelta automatica del computer
+
+#se scelta del computer = scelta ujtente > PAREGGIO
+# se scelta utente = sasso e computer = carta > WIN COMPUTER 
+# e via dicendo 
