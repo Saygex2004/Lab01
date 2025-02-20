dfe1e5dcbb73ae40b2f33ec0324b56f2d4b6e2e1
EdoLucca <edoardolucca1@gmail.com>
2025-02-20 14:15:52 +0100

    choice

--- main.py
+++ main.py
@@ -11,11 +11,24 @@ def game():
         print("choose: rock, paper, scissor or exit to quit")
         user_choice = input("Your choice: ")
 
-        #if user_choice
+        if user_choice == "rock":
+            ...
+
+        elif user_choice == "paper":
+            ...
+
+        elif user_choice == "scissor":
+            ...
 
 
 #eseguo la scelta automatica del computer
 
+    random.choice["rock","paper","scissor"]
+
 #se scelta del computer = scelta ujtente > PAREGGIO
-# se scelta utente = sasso e computer = carta > WIN COMPUTER 
+
+
+# se scelta utente = sasso e computer = carta > WIN COMPUTER
+
+
 # e via dicendo 
